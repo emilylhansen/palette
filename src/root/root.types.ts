7f7bbb59eb4c97b528b64f25027d8c8b9233cd94
emilylhansen <emilyhansen14@gmail.com>
2020-02-03 11:29:57 -0500

    add types

--- src/root/root.types.ts
+++ src/root/root.types.ts
@@ -2,4 +2,48 @@ import { AuthReducerState } from "/Users/emilyhansen/Desktop/palette-app/src/aut
 
 export type RootState = {
   auth: AuthReducerState;
+  palettesById: Record<string, Palette>;
+  colorsById: Record<string, Color>;
+  usersById: Record<string, User>;
+  favoritedPaletteIds: Record<string, boolean>;
+  favoritedColorIds: Record<string, boolean>;
+};
+
+export type Color = {
+  name: string;
+  description: string;
+  hex: string;
+  private: boolean;
+  key: string;
+};
+
+export type Tag = {
+  name: string;
+  key: string;
+};
+
+export type Palette = {
+  name: string;
+  description: string;
+  colors: Array<Color>;
+  private: boolean;
+  authorId: string;
+  key: string;
+  tags: Array<Tag>;
+};
+
+export type User = {
+  name: string;
+  description: string;
+  avatar: string;
+  key: string;
+  email: string;
+  createOn: Date;
+};
+
+export type Activity = {
+  userKey: string;
+  entityKey: string;
+  date: Date;
+  activity: "favorite" | "share";
 };
