75df6e0dd1fed7ef300064a04b961489340adc4f
emilylhansen <emilyhansen14@gmail.com>
2020-03-26 19:39:53 -0400

    fix routes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "src/root/App.tsx";
 
-/** Idk what's going on but this isn't correct. */
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
